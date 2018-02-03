33d30edf3d14e21d5501c94f242d0e1dde9d7c0d
Lauren Baker <spexbakerx@gmail.com>
2018-02-02 18:37:19 -0800

    trying to figure out this js

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// your code here!
+
+
+count + return number of words
+
+
+count + return unique words
+
+
+should produce average word length = get length of all words / by number of words
+
+
+
+
+
+function stripText(rawString) {
+  // NB: `.filter(Boolean)` removes any falsy items from an array
+  return rawString.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
+}
+
+function numWords(text) {
+	stripText(text);
+       $("#btnCount").click(function() {
+            var strText = jQuery.trim($('#txtArea').val());
+            alert('Total No of Words are: ' + strText.split(/\s+/).length);
+            });
+        });  
+
+
+function allTheThings(text) {
+	numWords(text);
+	uniqueWords(text);
+	wordLength(text);
+	 
+}
+
+
+
+$(function() {
+  allthethings();
+});
+
